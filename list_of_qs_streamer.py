4e440afaa85fd8753f555a418b271e8ad7a96c07
Ap1075 <armaanpuri17@gmail.com>
2019-02-11 12:52:47 +0530

    minor changes.

--- list_of_qs_streamer.py
+++ list_of_qs_streamer.py
@@ -12,15 +12,18 @@ import time
 import sched
 import configparser
 from multiprocessing import Event 
+import signal
 
 class Streamer():
 	
-	def __init__(self, directory, wait, list_length):
+	def __init__(self, directory, wait, list_length, final_q):
 		self.directory = directory
 		self.wait = wait
 		self.list_length = list_length
+		self.final_q = final_q
 	
 	def simpler(self, stream_name, f_q,  wait, list_length):
+		# signal.signal(signal.SIGINT, signal.SIG_IGN)
 		known_latest = []
 		path = str(stream_name) #.get()
 		multiprocessing.current_process().name = path
@@ -36,7 +39,7 @@ class Streamer():
 					if str(newest_file) not in known_latest:
 						known_latest.append(str(newest_file))
 						f_q.put(newest_file)
-						print("writing to queue done, releasing lock")
+						print("writing to queue done for {}".format(multiprocessing.current_process().name))
 						# print("here's the output", f_q)
 					else:
 						print("waiting for {} file now...".format(multiprocessing.current_process().name))
@@ -54,6 +57,7 @@ class Streamer():
 		return
 
 	def indefinite_checker(self, streams, n, final_q):
+		# signal.signal(signal.SIGINT, signal.SIG_IGN)
 		multiprocessing.current_process().name = "indefinite checker"
 		print(multiprocessing.current_process())
 		try:
@@ -78,72 +82,36 @@ class Streamer():
 		myStreams = [self.directory+"/"+'{}'.format(element) for element in myStreams]
 
 		n = len(os.listdir(self.directory))   #for each stream, a process would be started 
-		final_q = []    # initializing n queues, for each stream, this list is the final OUTPUT
+		self.final_q = []    # initializing n queues, 1 for each stream, this list is the final OUTPUT
 		for i in range(n):
-			final_q.append(multiprocessing.Queue())
-
+			self.final_q.append(multiprocessing.Queue())
 
 		workers = []
 		processes = {}
 		m=0
-		
-		# n=int(return_num.value)
+
 
 		for i in range(n):
-			work = multiprocessing.Process(target=self.simpler, args=(myStreams[i],final_q[i], self.wait, self.list_length)) 
+			work = multiprocessing.Process(target=self.simpler, args=(myStreams[i],self.final_q[i], self.wait, self.list_length)) 
 			work.start()
 			processes[n] = (work, i)
 			m+=1
 			workers.append(work)
 
-		checker = multiprocessing.Process(target=self.indefinite_checker, args=(myStreams,n, final_q))
+		checker = multiprocessing.Process(target=self.indefinite_checker, args=(myStreams, n, self.final_q))
 		checker.start()
-		
-		# for each in workers:
-		#     print("Let's join workers")
-		#     each.join()
-
-		# while len(processes) > 0:
-		#     for x in processes.keys():
-		#         (p, a) = processes[x]
-		#         time.sleep(0.5)
-		#         if p.exitcode is None and not p.is_alive(): # Not finished and not running
-		#              # Do your error handling and restarting here assigning the new process to processes[n]
-		#              print(a, 'is gone as if never born!')
-		#         elif p.exitcode < 0:
-		#             print ('Process Ended with an error or a terminate', a)
-		#             # Handle this either by restarting or delete the entry so it is removed from list as for else
-		#         else:
-		#             print (a, 'finished')
-		#             p.join() # Allow tidyup
-		#             del processes[x] # Removed finished items from the dictionary 
-		#             # When none are left then loop will end
-
-		# for worker in workers:
-		#     check = multiprocessing.Process(target=streamer.check_status, args=(worker,))
-		#     check.start()
-	  
-		# for i in range(n):
-		#     print("Final queue containing queues for each stream: ", final_q.get())
-		# event = Event()
-		
-		# signal.signal()
-		# 	event.set()
-		# 	streamer.spawner(event, new_stream_path, final_q)
 
 		for worker in workers:
 			if not worker.is_alive():
-				print("Joining the process before restarting.\n")
+				print("Joining dead {} *****\n".format(worker.name))
 				worker.join()
-				time.sleep(2)
-				print("Restarting process.")
-				worker.start()
-				continue
-			else:
-				pass
 
-if __name__=='__main__':
+def call_streamer():
 	config = configparser.ConfigParser()
 	config.read('./config.ini')
-	streamer = Streamer(config["DEFAULT"]["path_to_rec"], int(config["DEFAULT"]["wait_time"]), int(config["DEFAULT"]["list_length"]))
+	streamer = Streamer(config["DEFAULT"]["path_to_rec"], int(config["DEFAULT"]["wait_time"]), int(config["DEFAULT"]["list_length"]), [])
 	streamer.execute()
+	# print(streamer.final_q) ## streamer.final_q is the list of queues	
+
+if __name__=='__main__':
+	call_streamer()
