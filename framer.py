8a65db18d65a71856b2696022314009fd2257f90
Prajwal Rao <prajwaljpj@gmail.com>
2019-02-11 10:02:54 +0530

    framer.py almost final

--- framer.py
+++ framer.py
@@ -2,82 +2,73 @@ import os
 import cv2
 import multiprocessing as mp
 
-class frame_generator(object):
-    def __init__(self, stream_queue, frame_q):
-        self.frame_q = frame_q
-        self.stream_queue = stream_queue
 
-    #@classmethod
-    def get_frames(self):
-        for stream_q in self.stream_queue:
-            fp = mp.Process(self.splitter, args=(self.stream_queue,))
-            fp.start()
-
-    def splitter(self, stream_data):
-        if stream_data == None:
-            return
-        stream_id ,stream_file = stream_data
-        success, frame = cv2.VideoCapture(stream_file)
-        while success:
-            self.frame_q.put((stream_id, frame))
-            success, frame = cv2.VideoCapture(stream_file)
-
-class frame_task(object):
-    def __init__(self, frame_queue, stream_queue):
-        self.frame_queue = frame_queue
-        self.stream_queue = stream_queue
-
-    def __call__(self):
-        stream_frame = self.stream_queue.get()
-        self.frame_queue.put(stream_frame)
-
-    def __str__(self):
-        return self.frame_queue
-
-class frame_generator2(mp.Process):
-    def __init__(self, input_queue, frame_queue):
-        self.input_queue = input_queue
-        self.frame_queue = frame_queue
+class framer(mp.Process):
+    def __init__(self,input_queue, output_queue):
+        mp.Process.__init__(self)
+        print("initialized init of multiprocess")
+        self.input_queue= input_queue
+        self.output_queue= output_queue
 
     def run(self):
         proc_name = self.name
+        print("Run initiated for proc: {}".format(proc_name))
         while True:
-           next_task = self.input_queue
-           print(proc_name, next_task)
-           self.input_queue.task_done()
+            stream_name, next_file = self.input_queue.get()
+            if next_file is None:
+                print("Exiting stream in {}".format(proc_name))
+                self.input_queue.task_done()
+                break
+            print(proc_name, next_file)
+            frames_gen = frame_generator(next_file)
+            for frame in frames_gen:
+                if frame is None:
+                    continue
+                # print(frame)
+                self.output_queue.put((stream_name, frame),)
+            self.input_queue.task_done()
+            return
 
 
+class frame_generator(object):
+    def __init__(self, input_data):
+        print("Initialised frame_generator (should happen only once)")
+        self.filename = input_data
+        self.cap = cv2.VideoCapture(self.filename)
+        print("filename: {}".format(self.filename))
+
+    def __iter__(self):
+        print("iter obj happen only once")
+        ret = True
+        while ret:
+            ret, frame = self.cap.read()
+            yield frame
 
 
+def fmr(inqueue, outqueue):
+    inlen = len(inqueue)
+
+    consumers = [framer(inqueue[x], outqueue[x]) for x in range(inlen)]
+    print(consumers)
+    [w.start() for w in consumers]
+    # while True:
+    #     print(outqueue[0].get())
+    return
+    # return (inqueue, outqueue)
 
 if __name__ == '__main__':
-    framer_result = mp.Queue()
+
     #stream_ids = os.listdir(videos_path)
     streams = 2
 
-    streamer_queue = [mp.Queue() for stream in range(streams)]
-    streamer_queue[0].put(('stream1', '/home/rbc-gpu/prajwal/multiprocess_yolo/bakvid/20180818_0100_16-18-20.mp4'))
-    streamer_queue[0].put(('stream1', '/home/rbc-gpu/prajwal/multiprocess_yolo/bakvid/20180818_0100_16-18-21.mp4'))
-    streamer_queue[1].put(('stream2', '/home/rbc-gpu/prajwal/multiprocess_yolo/bakvid/20180818_0100_16-18-22.mp4'))
-    streamer_queue[1].put(('stream2', '/home/rbc-gpu/prajwal/multiprocess_yolo/bakvid/20180818_0100_16-18-23.mp4'))
+    framer_result = [mp.Queue() for stream in range(streams)]
+    inqueuelist = [mp.Queue() for stream in range(streams)]
+    inqueuelist[0].put(('stream1', '/home/prajwaljpj/projects/multiyolo/video/bakvid/20180818_0100_16-18-20.mp4'))
+    inqueuelist[0].put(('stream1', '/home/prajwaljpj/projects/multiyolo/video/bakvid/20180818_0100_16-18-21.mp4'))
+    inqueuelist[1].put(('stream2', '/home/prajwaljpj/projects/multiyolo/video/bakvid/20180818_0100_16-18-22.mp4'))
+    inqueuelist[1].put(('stream2', '/home/prajwaljpj/projects/multiyolo/video/bakvid/20180818_0100_16-18-23.mp4'))
 
+    fmr(inqueuelist, framer_result)
 
+    print(framer_result[0].get())
 
-
-
-
-
-
-    framz = frame_generator(streamer_queue, framer_result)
-    #streamer = Streamer(folder_path)
-    #streams = mp.Process(Streamer.get_files)
-    framz.get_frames()
-    #streams.start()
-    #streams.join()
-
-    while True:
-        frame_meta = framer_result.get()
-        if frame_meta == None:
-            break
-        st_id, frame_data = frame_meta
-        print(st_id)
